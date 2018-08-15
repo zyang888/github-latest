20db44754e429196e9360830071d5ef0dfd902f4
Zhengtang Yang <zyang888@uw.edu>
2018-08-14 23:30:46 -0700

    added lesson06 activity

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ import requests
 # (or another user name)
 
 if __name__ == "__main__":
-    username = sys.argv[1]
+    username = 'zyang888'
 
     # TODO:
     #
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
 
-    print("COMPLETE THE TODOs")
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    events = json.loads(response.content)
+
+    print(events[0]['created_at'])
     
 
 
