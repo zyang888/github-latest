f20a90ebdd08b480b894deb4a5a7b9c12a8c4490
Zhengtang Yang <zyang888@uw.edu>
2018-08-17 06:42:09 -0700

    Added lesson07 asssignment

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from myblog.models import Post, Category
 
+class CategoryInline(admin.TabularInline):
+	model = Category
+
+class PostAdmin(admin.ModelAdmin):
+	inlines = [
+		CategoryInline,
+		]
+# create an InlineModelAdmin to represent Categories on the Post admin view
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
 admin.site.register(Post)
 admin.site.register(Category)
+
